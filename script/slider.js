29fd0c4579f61f302f2da0e33def632fe316e780
Oleg Fedorov <akademuk24@gmail.com>
2022-10-07 17:06:14 +0300

    ss

--- script/slider.js
+++ script/slider.js
@@ -10,7 +10,7 @@ $('.media').slick({
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -19,14 +19,14 @@ $('.media').slick({
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 375,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
         }
       }
     ]
