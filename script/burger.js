19e543431709135c43e4348d257718bb43721de5
Oleg Fedorov <akademuk24@gmail.com>
2022-10-11 04:00:13 +0300

    ss

--- script/burger.js
+++ script/burger.js
@@ -1,11 +1,11 @@
 $(".birger-button").on("click", function(){
   $(".birger-button").toggleClass("close-btn");
   $(".menu").toggleClass("open-menu");
-  $('.body').addClass('overflow');
 });
 $(".birger-button").click(function(){
-  $(".body").addClass("bodyActive");
-  $(".birger-button").click(function(){
-    $('.body').removeClass('overflow');
-  });
+  if ($('.body').hasClass('overflow')) {
+    $(".body").removeClass("overflow");
+  } else {
+    $(".body").addClass("overflow");
+ }
 });
