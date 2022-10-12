e501ccc93bda3f4ca47025fca6c739b1ac7f21aa
Oleg Fedorov <akademuk24@gmail.com>
2022-10-12 10:54:15 +0300

    sss

--- script/menu.js
+++ script/menu.js
@@ -1,10 +1,11 @@
 $('.open').click(function() {
 	$('.menuRight').addClass('open_menu');
     $('.body').addClass('overflow');
-
+    $('.open').addClass('textHover');
 });
 
 $('.close').click(function() {
 	$('.menuRight').removeClass('open_menu');
     $('.body').removeClass('overflow');
+    $('.open').removeClass('textHover');
 });
